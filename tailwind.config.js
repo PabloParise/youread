3eb6088528a3f0084956afa604fa87f13584ec7c
Pablo Parise <pabloeparise@gmail.com>
2022-06-03 10:27:08 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  mode: 'jit',
+  content: [
+    './src/**/*.html',
+    './src/**/*.js',
+    './public/**/*.html',
+    './public/**/*.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
