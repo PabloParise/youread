8b7b8e6747556758ad635f4b95a3fdaf534db2c1
Pablo Parise <pabloeparise@gmail.com>
2022-06-15 12:41:45 -0300

    color changes

--- src/pages/searchBook.js
+++ src/pages/searchBook.js
@@ -32,7 +32,7 @@ const SearchBook = () => {
                            bg-blue-900  text-white
                              ">
                     <article className="flex flex-col items-center mt-20 mx-2 mb-6 p-4 h-fit
-                                     bg-blue-900 border-2 rounded-lg w-5/6 max-w-5xl
+                                     bg-slate-900 border-2 rounded-lg w-5/6 max-w-5xl
                                       sm:flex-row sm:items-start sm:justify-around">
                         <div className="">
                             <p className="bg-green-400 rounded-sm mb-1 text-center">
@@ -41,7 +41,7 @@ const SearchBook = () => {
                             <img className="" src={thumbnail} alt='book cover'/>
                         </div>
                         <div className={`mt-2 sm:mt-0 sm:ml-2 lg:max-w-lg`}>
-                            <div className="bg-blue-800 rounded-lg p-1">
+                            <div className="bg-slate-800 rounded-lg p-1">
                                 <h2 className="font-serif text-lg font-bold lg:text-2xl xl:text-4xl 2xl:text-5xl">
                                     {title.toUpperCase()}
                                 </h2>
@@ -49,7 +49,7 @@ const SearchBook = () => {
                                     by {author}
                                 </h3>
                             </div>
-                            <p className="bg-blue-900 rounded-lg p-1 mt-2 text-justify lg:text-lg xl:text-xl">
+                            <p className="bg-slate-900 rounded-lg p-1 mt-2 text-justify lg:text-lg xl:text-xl">
                                 {(() => {
                                     if(description === "") {
                                         return `${'> '}Description not available (but it's an amazing book!)`;
@@ -68,7 +68,7 @@ const SearchBook = () => {
                                                    onClick={() => {setShowLinks(!showLinks)}}>
                                     <p>Details</p><FaAngleDown />
                                 </button>
-                                <ul className={`list-inside list-disc bg-blue-800 rounded-lg p-1 mt-1 ${showLinks ? 'block': 'hidden'}`}>
+                                <ul className={`list-inside list-disc bg-slate-800 rounded-lg p-1 mt-1 ${showLinks ? 'block': 'hidden'}`}>
                                     <li>Publisher: {publisher}</li>
                                     <li>Category: {category}</li>
                                     <li>Pages: {pageCount}</li>
